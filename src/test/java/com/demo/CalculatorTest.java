049f07b2b5da4fbc8bc05411f1d551444572a24e
Karan00791 <129988539+Karan00791@users.noreply.github.com>
2026-07-06 20:26:26 +0530

    Create CalculatorTest.java

---
+++ src/test/java/com/demo/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.demo
+  import org.unit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class CalculatorTest {
+
+@Test
+  public void testAddition(){
+    Calculator calc = new Calculator();
+    assertEquals(5, calc.add(2,3));
+  }
+}
